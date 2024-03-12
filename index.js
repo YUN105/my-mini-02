895416a778c049b1108f3e5b8880f97f511f96e0
YUN105 <a0955894356@gmail.com>
2024-03-12 14:29:18 +0800

    init

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
-import express from "express";
-import { resolve } from "path";
-const __dirname = import.meta.dirname;
+//改成commomJS寫法
+const express = require("express");
+const resolve = require("path").resolve;
+// const __dirname = import.meta.dirname;
 
 const app = express();
 app.use(express.static(resolve(__dirname, "public")));
 
+app.use(
+  "/bootstrap",
+  express.static(resolve(__dirname, "node_modules/bootstrap/dist"))
+);
+app.use(
+  "/fontawesome",
+  express.static(
+    resolve(__dirname, "node_modules/@fortawesome/fontawesome-free")
+  )
+);
+app.use(
+  "/jquery",
+  express.static(resolve(__dirname, "node_modules/jquery/dist"))
+);
 app.get("/", (req, res) => {
   res.send("網站首頁");
 });
