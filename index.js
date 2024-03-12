d7695b41d7ec3b53c2ea63ba4f9d413b899fbf8b
YUN105 <a0955894356@gmail.com>
2024-03-12 14:03:11 +0800

    init

---
+++ index.js
@@ -0,0 +1,14 @@
+import express from "express";
+import { resolve } from "path";
+const __dirname = import.meta.dirname;
+
+const app = express();
+app.use(express.static(resolve(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.send("網站首頁");
+});
+
+app.listen(3000, () => {
+  console.log("伺服器已啟動於 http://localhost:3000");
+});
